7818ba758474d12e419b579b402bfec5253eb7d3
Elkerr <julen.elcoro@opendeusto.es>
2018-06-03 19:45:57 +0200

    estadisticas

--- LN/operaciones.cpp
+++ LN/operaciones.cpp
@@ -3,48 +3,10 @@
 
 
 
-void OrdenarAhorcadoPorPGanadas(vector <PAhorcado> p)
-{
-	PAhorcado p1;
-	int k, i, z;
-	for(i=0; i<p.size()-1;i++)
-	{
-		for(k=i, z=i+1;z<p.size();z++)
-		{
-			if(p[z].getPGanadas()>p[k].getPGanadas())
-			{
-				k=z;
-			}
-		}
-		if(k!=i)
-		{
-			p1=p[i];
-			p[i]=p[k];
-			p[k]=p1;
-		}
-	}
-}
-void OrdenarCalculadoraPorPuntMax(vector <PCalc> p)
-{
-	PCalc p1;
-	int k, i, z;
-	for(i=0; i<p.size()-1;i++)
-	{
-		for(k=i, z=i+1;z<p.size();z++)
-		{
-			if(p[z].getPMax()>p[k].getPMax())
-			{
-				k=z;
-			}
-		}
-		if(k!=i)
-		{
-			p1=p[i];
-			p[i]=p[k];
-			p[k]=p1;
-		}
-	}
-}
+
+bool pganadas(PAhorcado i, PAhorcado j)	{return(i.getPGanadas()>j.getPGanadas());}
+
+bool pmax(PCalc i, PCalc j)	{return(i.getPMax()>j.getPMax());}
 
 bool compNombre(Juego i, Juego j)	{return(i.getNombre().compare(j.getNombre())<0);}
 
@@ -53,3 +15,7 @@ bool compPrecio(Juego i, Juego j)	{return(i.getPrecio()>j.getPrecio());}
 void OrdenarJuegoPorNombre(vector <Juego> &j)	{sort(j.begin(),j.end(),compNombre);}
 
 void OrdenarJuegoPorPrecio(vector <Juego> &j)	{sort(j.begin(),j.end(),compPrecio);}
+
+void OrdenarAhorcadoPorPGanadas(vector <PAhorcado> &p) {sort(p.begin(),p.end(),pganadas);}
+
+void OrdenarCalculadoraPorPuntMax(vector <PCalc> &p) {sort(p.begin(),p.end(),pmax);}
--- LN/operaciones.h
+++ LN/operaciones.h
@@ -18,9 +18,13 @@
 
 void OrdenarJuegoPorNombre(vector <Juego> &j);
 void OrdenarJuegoPorPrecio(vector <Juego> &j);
-void OrdenarAhorcadoPorPGanadas(vector <PAhorcado> p);
-void OrdenarCalculadoraPorPuntMax(vector <PCalc> p);
+void OrdenarAhorcadoPorPGanadas(vector <PAhorcado> &p);
+void OrdenarCalculadoraPorPuntMax(vector <PCalc> &p);
 bool compPrecio(Juego i, Juego j);
 bool compNombre(Juego i, Juego j);
+bool compNpganadasombre(PAhorcado i, PAhorcado j);
+bool pmax(PCalc i, PCalc j);
+
+
 
 #endif
